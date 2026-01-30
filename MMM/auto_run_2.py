d21651cc2a40e47cbae8663d0311a168e1c00e23
root <shoyoshikawa2313@gmail.com>
2026-01-30 04:23:06 +0000

    1_30

--- MMM/auto_run_2.py
+++ MMM/auto_run_2.py
@@ -13,15 +13,15 @@ class Args:
         self.resume = resume
 
 process_composits = [
-    #Args(model="Long_Qwen",composition_file="composition2.yaml",root_output_dir="results",dir_name="Long_Qwen",seed=0),
+    Args(model="BioT5x2",composition_file="composition2.yaml",root_output_dir="results",dir_name="BioT5_mono",seed=1),
     #Args(model="JNK3",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_jnk3_1",seed=0),
     #Args(model="QED",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_QED_2",seed=1),
     #Args(model="DRD2",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_DRD2_2",seed=1),
     #Args(model="GSK3B",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_GSK3B_2",seed=1),
-    Args(model="Mestranol",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Mestranol_2",seed=1),
-    Args(model="Thiothixene",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Thiothixene_2",seed=1),
-    Args(model="Perindopril",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Perindopril_2",seed=1),
-    Args(model="Isomer",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Isomer_2",seed=1),
+    #Args(model="Mestranol",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Mestranol_2",seed=1),
+    #Args(model="Thiothixene",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Thiothixene_2",seed=1),
+    #Args(model="Perindopril",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Perindopril_2",seed=1),
+    #Args(model="Isomer",composition_file="composition2.yaml",root_output_dir="results",dir_name="random_Isomer_2",seed=1),
 ]
 
 for process_composit in process_composits:
@@ -48,5 +48,4 @@ for process_composit in process_composits:
 
     hours = (end_time - start_time) / 3600.0
     print('---- The whole process takes %.2f hours ----' % (hours))
-
     print(f"process {process_composit.model} finished !!")
